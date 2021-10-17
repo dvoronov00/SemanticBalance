c3f6505bc14d117a87cdd072eb5cb53f2478f474
Dmitriy Voronov <d.voronov@finch-melrose.com>
2021-10-17 22:36:47 +0300

    Small refactoring TrackUserProfileInteractor

--- app/src/main/java/com/dvoronov00/semanticbalance/domain/usecase/analytic/TrackUserProfileInteractor.kt
+++ app/src/main/java/com/dvoronov00/semanticbalance/domain/usecase/analytic/TrackUserProfileInteractor.kt
@@ -1,31 +1,22 @@
 package com.dvoronov00.semanticbalance.domain.usecase.analytic
 
-import com.dvoronov00.semanticbalance.data.calculateExistingDays
-import com.dvoronov00.semanticbalance.data.toFormattedDate
+import com.dvoronov00.semanticbalance.data.extension.calculateExistingDays
+import com.dvoronov00.semanticbalance.data.extension.toFormattedDate
 import com.dvoronov00.semanticbalance.domain.AnalyticKey
-import com.dvoronov00.semanticbalance.domain.Const
 import com.dvoronov00.semanticbalance.domain.model.Account
 import com.onesignal.OneSignal
 import com.yandex.metrica.YandexMetrica
 import com.yandex.metrica.profile.Attribute
 import com.yandex.metrica.profile.UserProfile
 import timber.log.Timber
-import java.text.SimpleDateFormat
-import java.time.format.DateTimeFormatter
 import java.util.*
 import javax.inject.Inject
 
 class TrackUserProfileInteractor @Inject constructor() {
     fun track(account: Account) {
         Timber.d("Track user profile: $account")
-        val calculateExistingDays = account.calculateExistingDays()
-        // TODO: Переделать логику выссчитывания balanceExpiredTime
-        val balanceExpiredTime = Calendar
-            .getInstance()
-            .apply {
-                add(Calendar.DAY_OF_MONTH, calculateExistingDays)
-            }.time
-            .time / 1000
+        val calculatedExistingDays = account.calculateExistingDays()
+        val balanceExpiredTime = getBalanceExpiredTimestamp(calculatedExistingDays)
 
         val userProfile = UserProfile.newBuilder()
             .apply {
@@ -35,7 +26,7 @@ class TrackUserProfileInteractor @Inject constructor() {
                 )
                 apply(
                     Attribute.customString(AnalyticKey.UserProfile.TARIFF)
-                        .withValue(account.tariffName ?: Const.UNDEFINED_VAL)
+                        .withValue(account.tariffName ?: UNDEFINED_VAL)
                 )
                 apply(
                     Attribute.customNumber(AnalyticKey.UserProfile.SUBSCRIPTION_FEE)
@@ -43,7 +34,7 @@ class TrackUserProfileInteractor @Inject constructor() {
                 )
                 apply(
                     Attribute.customNumber(AnalyticKey.UserProfile.EXISTING_DAYS)
-                        .withValue(calculateExistingDays.toDouble())
+                        .withValue(calculatedExistingDays.toDouble())
                 )
                 apply(
                     Attribute.customNumber(AnalyticKey.UserProfile.BALANCE_WILL_EXPIRED)
@@ -61,8 +52,22 @@ class TrackUserProfileInteractor @Inject constructor() {
 
         OneSignal.sendTag("user_id", account.id.toString().trim())
         OneSignal.sendTag("user_balance", account.balance?.trim())
-        OneSignal.sendTag("user_existing_days", calculateExistingDays.toString().trim())
+        OneSignal.sendTag("user_existing_days", calculatedExistingDays.toString().trim())
         OneSignal.sendTag("user_tariff", account.tariffName?.trim())
         OneSignal.sendTag("user_balance_will_expired", balanceExpiredTime.toString())
+    }
+
+    private fun getBalanceExpiredTimestamp(existingDays: Int): Long = Calendar
+        .getInstance()
+        .apply {
+            set(Calendar.HOUR, 12)
+            set(Calendar.MINUTE, 10)
+            set(Calendar.SECOND, 0)
+            add(Calendar.DAY_OF_MONTH, existingDays)
+        }.time
+        .time / 1000
+
+    companion object {
+        private const val UNDEFINED_VAL = "Неизвестное значение"
     }
 }

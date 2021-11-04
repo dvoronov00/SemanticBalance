5fbf504b2be5248b41fbf5a929c66ce859ff7782
Dmitriy Voronov <d.voronov@finch-melrose.com>
2021-11-04 22:12:59 +0300

    Refactored AccountRepository

--- app/src/main/java/com/dvoronov00/semanticbalance/domain/parser/AccountParser.kt
+++ app/src/main/java/com/dvoronov00/semanticbalance/domain/parser/AccountParser.kt
@@ -1,12 +1,7 @@
 package com.dvoronov00.semanticbalance.domain.parser
 
-import com.dvoronov00.semanticbalance.domain.model.Service
+import com.dvoronov00.semanticbalance.domain.model.Account
 
 interface AccountParser {
-    fun getId() : Int
-    fun getBalance(): String
-    fun getTariffName(): String
-    fun getSubscriptionFee() : String
-    fun getAccountState() : String
-    fun getServices() : List<Service>
+    fun getAccount(): Account
 }

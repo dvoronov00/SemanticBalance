1f697775b5b4f54114e0eb8ee2c94810f77c789a
Dmitriy Voronov <d.voronov@finch-melrose.com>
2021-11-04 22:11:56 +0300

    Refactored NewsRepositoryImpl.kt

--- app/src/main/java/com/dvoronov00/semanticbalance/data/repository/NewsRepositoryImpl.kt
+++ app/src/main/java/com/dvoronov00/semanticbalance/data/repository/NewsRepositoryImpl.kt
@@ -1,8 +1,7 @@
 package com.dvoronov00.semanticbalance.data.repository
 
-import com.dvoronov00.semanticbalance.data.parser.NewsParserImpl
+import com.dvoronov00.semanticbalance.data.parser.NewsHtmlParserImpl
 import com.dvoronov00.semanticbalance.domain.model.News
-import com.dvoronov00.semanticbalance.domain.parser.NewsParser
 import com.dvoronov00.semanticbalance.domain.remote.SemanticConnection
 import com.dvoronov00.semanticbalance.domain.repository.NewsRepository
 import io.reactivex.rxjava3.core.Observable
@@ -12,13 +11,10 @@ class NewsRepositoryImpl @Inject constructor(
     private val semanticConnection: SemanticConnection
 ) : NewsRepository {
 
-
     override fun getNews(): Observable<List<News>> {
         return semanticConnection.getNewsHtml()
-            .map {
-                val parser : NewsParser = NewsParserImpl(it)
-                parser.getNews()
+            .map { html ->
+                NewsHtmlParserImpl(html).getNews()
             }
     }
-
 }
